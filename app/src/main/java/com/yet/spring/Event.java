08d22fb4b4bf3d1330fbb57a5c7b7d45b6f5e194
Moseichuk Alexandr <moseichuk.alexandr@gmail.com>
2021-03-26 01:15:59 +0300

    initial commit

---
+++ app/src/main/java/com/yet/spring/Event.java
@@ -0,0 +1,42 @@
+package com.yet.spring;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class Event {
+    private int id = new Random().nextInt();
+    private String msg;
+    private Date date;
+    private DateFormat df;
+
+    public Event(Date date, DateFormat df) {
+        this.date = date;
+        this.df = df;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", msg='" + msg + '\'' +
+                ", date=" + df.format(date) +
+                "}\n";
+    }
+}
